40fcbf5522eaaa7e82d51cbf83139a6f0fe8ca9a
Jethro <git@jethromuller.co.za>
2016-09-13 14:50:47 +0200

    Supress warnings during processing. Add test/train data split

--- chw_data.py
+++ chw_data.py
@@ -1,37 +1,23 @@
+from util import filter_warnings
+
 from pandas import DataFrame
 from sklearn.preprocessing import LabelEncoder, normalize
+from sklearn.cross_validation import train_test_split
 
 
 class CHWData(object):
     def __init__(self, file_name, label='', drop_cols=list(),
                  categorical_features=list()):
+        filter_warnings()
         self._file_name = file_name
-        self._dataset = DataFrame.read_csv(file_name)
+        self._dataset = DataFrame.from_csv(file_name)
         self._features = self._dataset.columns
         self._processed_dataset = None
-        self.label = label
         self.drop_cols = drop_cols
         self.categorical_cols = categorical_features
-
-    @property
-    def dataset(self):
-        return self._processed_dataset or self._dataset
-
-    @property
-    def features(self):
-        return self.dataset[self._features]
-
-    @property
-    def targets(self):
-        return self.dataset[self.label]
-
-    @property
-    def features_m(self):
-        return self.features.as_matrix()
-
-    @property
-    def targets_m(self):
-        return self.targets.as_matrix()
+        self.label = label
+        if label:
+            self._process_dataset(label, True, drop_cols, categorical_features)
 
     def _process_dataset(self, label, assign=True, drop_cols=list(),
                          categorical_features=list()):
@@ -52,3 +38,39 @@ class CHWData(object):
             self._features = features
 
         return dataset
+
+    def update(self, label='', drop_cols=list(), categorical_features=list()):
+        self._process_dataset(label, True, drop_cols, categorical_features)
+
+    @property
+    def dataset(self):
+        if self._processed_dataset is not None:
+            return self._processed_dataset
+        return self._dataset
+
+    @property
+    def feature_labels(self):
+        return self._features
+
+    @property
+    def features(self):
+        return self.dataset[self._features]
+
+    @property
+    def targets(self):
+        return self.dataset[self.label]
+
+    @property
+    def features_m(self):
+        return self.features.as_matrix()
+
+    @property
+    def targets_m(self):
+        return self.targets.as_matrix()
+
+    def get_test_train_data(self, train_portion):
+        return train_test_split(self.features, self.targets,
+                                train_size=train_portion)
+
+    def get_test_train_data_m(self, train_portion):
+        return [i.as_matrix() for i in self.get_test_train_data(train_portion)]

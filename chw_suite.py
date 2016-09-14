b760f81c7682dac9e08f6a553b28135add544204
Jethro <git@jethromuller.co.za>
2016-09-14 23:59:02 +0200

    Add plotting for the first accuracy comparison

--- chw_suite.py
+++ chw_suite.py
@@ -2,7 +2,6 @@ from chw_data import CHWData
 
 import matplotlib.pyplot as plt
 import matplotlib
-matplotlib.rcParams['backend'] = "Qt4Agg"
 
 from sklearn.cross_validation import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
@@ -10,6 +9,8 @@ from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sknn.mlp import Classifier, Layer
 
+
+matplotlib.rcParams['backend'] = "Qt4Agg"
 label = 'activeQ2'
 unused_features = ['projectCode', 'userCode']
 categorical_features = ['country', 'sector']
@@ -30,33 +31,35 @@ estimators = {'Decision Tree': tree, 'Random Forest': forest,
               'SVM': svm, 'Neural Network': nn}
 
 
-def param_run(num_x, cross_folds):
+def param_run(num_x, cross_folds, drop_cols=list()):
     results = []
-    for name, estimator in estimators.iteritems():
-        scores = cross_val_score(estimator, chw_data.get_features(num_x, True),
-                                 chw_data.targets_m,
-                                 cv=cross_folds)
-        print '[%d] %s Accuracy: %0.2f (+/- %0.2f)' % (num_x, name,
-                                                       scores.mean(),
-                                                       scores.std() * 2)
-        results.append((name, scores.mean()))
+    for estimator_name, estimator in estimators.iteritems():
+        feature_data = chw_data.get_features(num_x, True).drop(drop_cols)
+        cross_score = cross_val_score(estimator, feature_data,
+                                      chw_data.targets_m, cv=cross_folds)
+        print '[%d] %s Accuracy: %0.2f (+/- %0.2f)' % (num_x, estimator_name,
+                                                       cross_score.mean(),
+                                                       cross_score.std() * 2)
+        results.append((estimator_name, cross_score.mean()))
     return results
-
 
 all_scores = {i: [] for i in estimators.keys()}
 # Go through all values of X (1-90)
 x_val_range = range(1, 91)
 for x in x_val_range:
-    results = param_run(x, cross_folds=2)
-    for result in results:
+    result_scores = param_run(x, cross_folds=10)
+    for result in result_scores:
         name, val = result
         all_scores[name].append(val)
 
 legend = []
+styles = iter(['-', '--', '-.', ':'])
+plt.ioff()
 for name, scores in all_scores.iteritems():
-    plt.plot(x_val_range, scores)
+    plt.plot(x_val_range, scores, next(styles))
     legend.append(name)
 
-plt.legend(legend)
-plt.show()
-
+plt.legend(legend, loc=4)
+plt.ylabel('Accuracy')
+plt.xlabel('Number of days included')
+plt.savefig('x-vals-compare.png')

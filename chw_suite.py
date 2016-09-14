33ebf721c9021d4b922dfe19682db6c2b440dff7
Jethro <git@jethromuller.co.za>
2016-09-14 14:29:01 +0200

    Add calls for the first graph

--- chw_suite.py
+++ chw_suite.py
@@ -1,5 +1,9 @@
 from chw_data import CHWData
 
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams['backend'] = "Qt4Agg"
+
 from sklearn.cross_validation import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
@@ -20,13 +24,39 @@ nn = Classifier(
     layers=[
         Layer('Rectifier', units=10),
         Layer('Softmax')],
-    learning_rate=0.02, n_iter=25, verbose=False)
+    learning_rate=0.02, n_iter=20, verbose=False)
 
 estimators = {'Decision Tree': tree, 'Random Forest': forest,
               'SVM': svm, 'Neural Network': nn}
 
-for name, estimator in estimators.iteritems():
-    scores = cross_val_score(estimator, chw_data.features_m,
-                             chw_data.targets_m, cv=5)
-    print '%s Accuracy: %0.2f (+/- %0.2f)' % (name, scores.mean(),
-                                              scores.std() * 2)
+
+def param_run(num_x, cross_folds):
+    results = []
+    for name, estimator in estimators.iteritems():
+        scores = cross_val_score(estimator, chw_data.get_features(num_x, True),
+                                 chw_data.targets_m,
+                                 cv=cross_folds)
+        print '[%d] %s Accuracy: %0.2f (+/- %0.2f)' % (num_x, name,
+                                                       scores.mean(),
+                                                       scores.std() * 2)
+        results.append((name, scores.mean()))
+    return results
+
+
+all_scores = {i: [] for i in estimators.keys()}
+# Go through all values of X (1-90)
+x_val_range = range(1, 91)
+for x in x_val_range:
+    results = param_run(x, cross_folds=2)
+    for result in results:
+        name, val = result
+        all_scores[name].append(val)
+
+legend = []
+for name, scores in all_scores.iteritems():
+    plt.plot(x_val_range, scores)
+    legend.append(name)
+
+plt.legend(legend)
+plt.show()
+

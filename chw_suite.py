2c45ade79c4d31e292ca843b620333467ea560a6
Jethro <git@jethromuller.co.za>
2016-09-16 17:45:56 +0200

    Turn experiment into function. Move graph drawing to function
    * Graph gets a set of N colours and cycles through 12 unique line formats.
    * Graph shows error bars if error is above min_err.
    * x and y axes have limits set by default.
    * Results of each step in the first experiment are logged to file for
      each classifier.

--- chw_suite.py
+++ chw_suite.py
@@ -1,26 +1,25 @@
+import itertools
 import json
 
 from chw_data import CHWData
+from util import generate_n_rgb_colours, round_up
 
 import matplotlib
 import matplotlib.pyplot as plt
-
 from sklearn.cross_validation import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
-
 from sknn.platform import gpu64
 from sknn.mlp import Classifier, Layer
 
 
 matplotlib.rcParams['backend'] = "Qt4Agg"
 label = 'activeQ2'
-unused_features = ['projectCode', 'userCode']
+drop_features = ['projectCode', 'userCode']
 categorical_features = ['country', 'sector']
 
-chw_data = CHWData('chw_data.csv', label,
-                   unused_features, categorical_features)
+chw_data = CHWData('chw_data.csv', label, drop_features, categorical_features)
 
 tree = DecisionTreeClassifier()
 forest = RandomForestClassifier()
@@ -35,14 +34,13 @@ estimators = {'Decision_Tree': tree, 'Random_Forest': forest,
               'SVM': svm, 'Neural_Network': nn}
 
 
-def param_run(num_x, cross_folds, drop_cols=list()):
+def param_run(num_x=90, cross_folds=10, drop_cols=list()):
     results = []
     for estimator_name, estimator in estimators.iteritems():
         print '[%d] Starting %s' % (num_x, estimator_name)
         feature_data = chw_data.get_features(num_x).drop(drop_cols)
         feature_data = (feature_data.as_matrix()
-                        if estimator_name == 'Neural Network'
-                        else feature_data)
+                        if estimator == nn else feature_data)
         cross_score = cross_val_score(estimator, n_jobs=-1,
                                       X=feature_data,
                                       y=chw_data.targets, cv=cross_folds)
@@ -52,26 +50,51 @@ def param_run(num_x, cross_folds, drop_cols=list()):
         results.append((estimator_name, cross_score))
     return results
 
-all_scores = {i: [] for i in estimators.keys()}
-# Go through all values of X (1-90)
-x_val_range = range(1, 91)
-for x in x_val_range:
-    result_scores = param_run(x, cross_folds=10)
-    for result in result_scores:
-        name, val = result
-        all_scores[name].append(val.mean())
-        with open('x-vals-%s-scores.json' % name, 'a+') as fout:
-            fout.write(json.dumps(val.tolist()) + '\n')
 
-legend = []
-styles = iter(['-', '--', '-.', ':'])
-plt.ioff()
-for name, scores in all_scores.iteritems():
-    plt.plot(x_val_range, scores, next(styles))
-    legend.append(name)
+def draw_graph(graph_scores, x_values, y_lim=(0, 1), x_lim=None,
+               y_label='', x_label='', file_name='', min_err=0.2):
+    legend = []
+    colours = iter(generate_n_rgb_colours(len(estimators)))
+    markers = ['o', '^', 's', 'D']
+    line_types = ['-', '--', ':']
+    styles = itertools.cycle(itertools.product(markers, line_types))
 
-plt.legend(legend, loc=4)
-plt.ylim(0, 1)
-plt.ylabel('Accuracy')
-plt.xlabel('Number of days included')
-plt.savefig('x-vals-compare.png')
+    x_lim = x_lim or (0, round_up(len(x_values)))
+
+    plt.ioff()
+    for graph_label, graph_vals in graph_scores.iteritems():
+        y_vals, y_err = graph_vals
+        y_err = y_err if any(map(lambda x: x > min_err, y_err)) else None
+        plt.errorbar(x_values, y_vals, yerr=y_err, fmt=next(styles),
+                     c=next(colours), linewidth=1.5, markersize=7,
+                     markeredgewidth=1)
+        legend.append(graph_label)
+
+    plt.legend(legend, loc=4)
+    plt.ylim(y_lim)
+    plt.xlim(x_lim)
+    plt.ylabel(y_label)
+    plt.xlabel(x_label)
+    plt.savefig(file_name)
+
+
+def effect_of_day_data_experiment():
+    all_scores = {i: [[], []] for i in estimators.keys()}
+    # Go through all values of X (1-90)
+    x_val_range = range(1, 91)
+    for x in x_val_range:
+        result_scores = param_run(num_x=x, cross_folds=10)
+        for result in result_scores:
+            name, val = result
+            all_scores[name][0].append(val.mean())
+            all_scores[name][1].append(val.std())
+            with open('x-vals-%s-scores.json' % name, 'a+') as fout:
+                fout.write(json.dumps(val.tolist()) + '\n')
+
+    draw_graph(all_scores, x_val_range, file_name='x-vals-compare.png',
+               y_label='Accuracy', x_label='Number of days included')
+
+
+if __name__ == '__main__':
+    print('Running Effect of Day Experiment')
+    effect_of_day_data_experiment()

b768e148b298f2beaac13c854d0b2722fc16c393
Jethro <git@jethromuller.co.za>
2016-09-19 22:50:15 +0200

    Add region data experiment. Misc updates
    * Add new region data experiment.
    * Add custom cross validation to enable new experiment. Uses joblib.
    * Update graph drawning and config generation to support region experiment.
    * Update param_run to support new experiment. Update debug prints to take
      some given label. Custom column filtering.
    * Draw error bars when std > 2%.
    * Add support for drawing separate graphs for each estimator in config.
    * Move graph style setup to allow for nicer graphs when split enabled.
    * Remove other line types meant for more estimators.
    * Get date just before writing out results instead of in each experiment function.
    * Clear pyplot figure when done drawing graph to properly support split option.

--- chw_suite.py
+++ chw_suite.py
@@ -6,66 +6,72 @@ import json
 from chw_data import CHWData
 from util import generate_n_rgb_colours, round_up
 
+from joblib import Parallel, delayed
 import matplotlib
 from matplotlib.ticker import MultipleLocator
 import matplotlib.pyplot as plt
 import numpy
 from numpy import ndarray
+from sklearn import clone
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
 
-def param_run(num_x=90, cross_folds=10, drop_cols=list()):
+def param_run(debug_label=None, num_x=90, cross_folds=10, col_select=None,
+              drop_cols=list(), test_data=None):
     results = []
+    debug_label = '[{}] '.format(debug_label if debug_label else '')
     for estimator_name, estimator in estimators.iteritems():
-        print '[%d] Starting %s' % (num_x, estimator_name)
-        feature_data = chw_data.get_features(num_x).drop(drop_cols)
-        feature_data = (feature_data.as_matrix()
-                        if estimator == nn else feature_data)
-        cross_score = cross_val_score(estimator, n_jobs=-1,
-                                      X=feature_data,
-                                      y=chw_data.targets, cv=cross_folds)
+        print '%sStarting %s' % (debug_label, estimator_name)
+        feature_data = chw_data.get_features(num_x, col_select).drop(drop_cols)
+        target_data = chw_data.get_targets(col_select)
+        score = cross_validate_score(estimator, feature_data, target_data,
+                                     test_data=test_data, cv=cross_folds)
         print '     %s Accuracy: %0.2f (+/- %0.3f)' % (estimator_name,
-                                                       cross_score.mean(),
-                                                       cross_score.std())
-        results.append((estimator_name, cross_score))
+                                                       score.mean(),
+                                                       score.std())
+        results.append((estimator_name, score))
     return results
 
 
 def draw_graph(graph_scores, x_values, y_lim=(0, 1), x_lim=None,
-               y_label='', x_label='', file_name='', min_err=0.2, grid=True):
+               y_label='', x_label='', file_name='', min_err=0.02, grid=True):
     legend = []
-    colours = iter(generate_n_rgb_colours(len(estimators)))
-    markers = ['o', '^', 's', 'D']
-    line_types = ['-', '--', ':']
-    styles = [''.join(i) for i in itertools.product(line_types, markers)]
-    styles = itertools.cycle(styles)
     x_lim = x_lim or (0, round_up(len(x_values)))
+
+    x_range = x_values
+    if type(x_values[0]) not in [int, float]:
+        x_range = range(1, len(x_values) + 1)
 
     plt.ioff()
     plt.grid(grid)
     for graph_label, graph_vals in graph_scores.iteritems():
         y_vals, y_err = graph_vals
         y_err = y_err if any(map(lambda x: x > min_err, y_err)) else None
-        plt.errorbar(x_values, y_vals, yerr=y_err, fmt=next(styles),
+        plt.errorbar(x_range, y_vals, yerr=y_err, fmt=next(styles),
                      c=next(colours), linewidth=1.5, markersize=7,
                      markeredgewidth=1)
         legend.append(graph_label.replace('_', ' '))
 
     plt.gca().yaxis.set_major_locator(MultipleLocator(base=0.1))
-    plt.gca().xaxis.set_major_locator(MultipleLocator(base=1.0))
+    if type(x_values[0]) in [int, float]:
+        plt.gca().xaxis.set_major_locator(MultipleLocator(base=1.0))
+    else:
+        plt.xticks(x_range, x_values, rotation=90)
+        plt.tight_layout()
     plt.legend(legend, loc=4)
     plt.ylim(y_lim)
     plt.xlim(x_lim)
     plt.ylabel(y_label)
     plt.xlabel(x_label)
     plt.savefig(file_name)
+    plt.clf()
 
 
-def draw_graph_from_file(experiment):
+def draw_graph_from_file(experiment, split=False):
     file_scores = {}
     with open('%s-config.json' % experiment) as config_file:
         config = json.loads(config_file.readline())
@@ -79,54 +85,105 @@ def draw_graph_from_file(experiment):
             file_scores[classifier][0].append(score.mean())
             file_scores[classifier][1].append(score.std())
 
-    draw_graph(file_scores, **config)
+    if split:
+        for key, val in file_scores.iteritems():
+            conf = {k: v if k != 'file_name' else '%s-%s' % (key, v)
+                    for k, v in config.iteritems()}
+            draw_graph({key: val}, **conf)
+    else:
+        draw_graph(file_scores, **config)
 
 
-def write_out_results(experiment, results, x_values, x_label, y_label, date,
+def write_out_results(experiment, results, x_values, x_label, y_label,
                       file_name=None, draw=True):
+    date = datetime.utcnow().replace(microsecond=0).isoformat()
     file_name = file_name or '%s-%s-graph.png' % (experiment, date)
-    agg_scores = {i: [[], []] for i in results.keys()}
-
-    for name, values in results.iteritems():
-        for val in values:
-            agg_scores[name][0].append(val.mean())
-            agg_scores[name][1].append(val.std())
 
     config = {
         'x_values': x_values, 'file_name': file_name, 'y_label': y_label,
         'x_label': x_label,
     }
 
+    results_list = {n: map(ndarray.tolist, results[n]) for n in results}
+    with open('%s-%s-config.json' % (experiment, date), 'w') as config_file:
+        config_file.write(json.dumps(dict(config, results=results_list)))
+
     if draw:
+        agg_scores = {i: [[], []] for i in results.keys()}
+        for name, values in results.iteritems():
+            for val in values:
+                agg_scores[name][0].append(val.mean())
+                agg_scores[name][1].append(val.std())
         draw_graph(agg_scores, **config)
 
-    config['results'] = {n: map(ndarray.tolist, results[n]) for n in results}
-    with open('%s-%s-config.json' % (experiment, date), 'w') as config_file:
-        config_file.write(json.dumps(config))
+
+def fit_and_score(estimator, feature_data, target_data, train_indices,
+                  test_indices, test_data=None):
+    features = feature_data.iloc[train_indices]
+    targets = target_data.iloc[train_indices]
+    estimator.fit(features, targets)
+
+    test_features = (test_data.features
+                     if test_data else feature_data).iloc[test_indices]
+    test_targets = (test_data.targets
+                    if test_data else target_data).iloc[test_indices]
+    return estimator.score(test_features, test_targets)
+
+
+def cross_validate_score(estimator, feature_data, target_data, test_data=None,
+                         cv=10):
+    kfold = StratifiedKFold(n_splits=cv)
+    parallel = Parallel(n_jobs=-1)
+    split = kfold.split(feature_data, target_data)
+    scores = parallel(delayed(fit_and_score)(
+        clone(estimator), feature_data, target_data, train, test, test_data)
+             for train, test in split)
+    return numpy.array(scores)
 
 
 def effect_of_day_data_experiment():
-    print('Running Effect of Day Experiment\n--------------------------------')
-    date = datetime.utcnow().replace(microsecond=0).isoformat()
+    title = 'Running Effect of Day Experiment'
+    print('%s\n%s' % (title, '-' * len(title)))
     out_results = {i: [] for i in estimators.keys()}
     # Go through all values of X (1-90)
     x_val_range = range(1, 91)
     for x in x_val_range:
-        result_scores = param_run(num_x=x, cross_folds=10)
+        result_scores = param_run(debug_label=x, num_x=x, cross_folds=10)
         for result in result_scores:
             name, val = result
             out_results[name].append(val)
 
     write_out_results('xvals', out_results, x_val_range,
-                      'Number of days included', 'Accuracy', date)
+                      'Number of days included', 'Accuracy')
+
+
+def region_generalization_experiment():
+    title = 'Running Region Generalization Experiment'
+    print('%s\n%s' % (title, '-' * len(title)))
+    region_data_size = 500
+    countries = chw_data._dataset.groupby('country').size().sort_values()
+    countries = countries[countries > region_data_size].keys().tolist()
+    out_results = {name: [] for name in estimators.keys()}
+
+    for country in countries:
+        col_select = {country: 1}
+        result_scores = param_run(debug_label=country, cross_folds=10,
+                                  col_select=col_select, test_data=chw_data)
+        for result in result_scores:
+            name, val = result
+            out_results[name].append(val)
+
+    write_out_results('region', out_results, countries, 'Country', 'Accuracy')
 
 
 if __name__ == '__main__':
     experiments = [
         '0. Effect of Number of Days Included (1-90)',
+        '1. Ability to Generalize Region Data',
     ]
     experiment_functions = [
         effect_of_day_data_experiment,
+        region_generalization_experiment
     ]
 
     parser = argparse.ArgumentParser()
@@ -135,11 +192,18 @@ if __name__ == '__main__':
                         help='Choose which experiments to run as list',)
     parser.add_argument('-g', '--graph', dest='graph_file', type=str,
                         help='Graph values from experiment',)
+    parser.add_argument('-s', '--split', action='store_true',
+                        help='Generate a separate graph for each estimator')
     parser.add_argument('-l', '--list', action='store_true',
                         help='List all experiments')
     args = parser.parse_args()
 
     matplotlib.rcParams['backend'] = "Qt4Agg"
+    markers = ['o', '^', 's', 'D']
+    line_types = ['-']
+    styles = [''.join(i) for i in itertools.product(line_types, markers)]
+    styles = itertools.cycle(styles)
+
     label = 'activeQ2'
     drop_features = ['projectCode', 'userCode']
     categorical_features = ['country', 'sector']
@@ -157,12 +221,14 @@ if __name__ == '__main__':
         'SVM': svm, 'Neural_Network': nn,
     }
 
+    colours = iter(generate_n_rgb_colours(len(estimators)))
+
     if args.list:
         print 'All Experiments:\n----------------'
         print '\n'.join(experiments)
     elif args.graph_file:
         print 'Drawing graph: %s' % args.graph_file
-        draw_graph_from_file(args.graph_file)
+        draw_graph_from_file(args.graph_file, args.split)
     elif not args.experiments:
         print 'Running All Experiments\n=======================\n'
         map(lambda func: func(), experiment_functions)
